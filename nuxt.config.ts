ae487e0454fd52607145892a80d71ca033bca742
Angus Gaukroger <agaukroger77@gmail.com>
2023-02-18 12:37:10 +0000

    Added Tailwind config.. works as normal now

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,15 @@
 export default defineNuxtConfig({
     modules: [
         '@nuxtjs/tailwindcss'
-      ]
+      ],
+    tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config.js',
+    exposeConfig: false,
+    injectPosition: 0,
+    viewer: true,
+    }
+      
     // vite: {
     //     server: {
     //       watch: {
